8b1afcbd345694150147054691577e5aae2041dd
LauraWillemsPXL <laura.willems@student.pxl.be>
2024-12-29 20:45:07 +0100

    US-05 Added tags to posts

--- backend-java/post-service/src/main/java/be/pxl/services/domain/dto/PostResponse.java
+++ backend-java/post-service/src/main/java/be/pxl/services/domain/dto/PostResponse.java
@@ -1,10 +1,12 @@
 package be.pxl.services.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @Builder
@@ -14,7 +16,9 @@ public class PostResponse {
     private Long id;
     private String title;
     private String content;
+    private List<String> tags;
     private String author;
     private Date publishedDate;
+    @JsonProperty("draft")
     private boolean isDraft;
 }
