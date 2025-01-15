d14f68c662e00a652c31af3a8229a167a637e028
LauraWillemsPXL <laura.willems@student.pxl.be>
2025-01-15 09:11:03 +0100

    Fixed bug in route guard

--- frontend-web/news-app/src/app/confirm-leave.guard.ts
+++ frontend-web/news-app/src/app/confirm-leave.guard.ts
@@ -2,6 +2,9 @@ import { CanDeactivateFn } from '@angular/router';
 import {AddPostFormComponent} from "./core/add-post-form/add-post-form.component";
 
 export const confirmLeaveGuard: CanDeactivateFn<AddPostFormComponent> = (component, currentRoute, currentState, nextState) => {
+  if (component.postForm.valid) {
+    return true;
+  }
   if (component.postForm.dirty) {
     return window.confirm('You have unsaved changes. Are you sure you want to leave?');
   }
